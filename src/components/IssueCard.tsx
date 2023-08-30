9e13df8e1fb7212ca9291edfd17ae1dde27aaa99
hodee <tlsghk8619@gmail.com>
2023-08-30 18:16:48 +0900

    feat: axios 세팅

---
+++ src/components/IssueCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IssueCard = () => {
+  return (
+    <div className="card border-solid border-2 border-indigo-300">
+      <span>#111</span>
+      <h3>Issuetitle</h3>
+    </div>
+  );
+};
+
+export default IssueCard;
--- src/pages/Issuelist.tsx
+++ src/pages/Issuelist.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import IssueCard from "../components/IssueCard";
 
 const Issuelist = () => {
-  return <main>list</main>;
+  return (
+    <main>
+      <IssueCard />
+      <IssueCard />
+      <IssueCard />
+      <IssueCard />
+    </main>
+  );
 };
 
 export default Issuelist;
