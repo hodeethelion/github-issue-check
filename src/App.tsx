3f7a169fc49a85bc081e8af6a474eb9cff0c5ae5
hodee <tlsghk8619@gmail.com>
2023-08-30 13:08:50 +0900

    feat: Issue 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Issuedetail from "./pages/Issuedetail";
+import Issuelist from "./pages/Issuelist";
 
 function App() {
-  return <div className="App">wat up</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Issuelist />} />
+      <Route path="/:id" element={<Issuedetail />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Issuedetail.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Issuedetail = () => {
+  const params = useParams();
+  // params: 오브젝트로 들어오는 데 id는 1이게 될 것 같음
+  return <div>detail</div>;
+};
+
+export default Issuedetail;
